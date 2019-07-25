1d0ce495a5df48ebe8f75f24041559f6aacbf3eb
Tommi Tuovinen <tommi.s.tuovinen@gmail.com>
2019-07-25 21:36:31 +0300

    Add a draft of Card component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.sass';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <h2>Todo</h2>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.sass'
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <p>Card</p>
+        </div>
+        )
+    }
+}
+
+export default Card
